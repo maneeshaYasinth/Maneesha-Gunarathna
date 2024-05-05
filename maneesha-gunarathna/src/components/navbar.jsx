28b8f3f5a17c4e5103835fc08676bc86f92f253c
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-05-05 21:28:18 +0530

    setup the navbar css

--- maneesha-gunarathna/src/components/navbar.jsx
+++ maneesha-gunarathna/src/components/navbar.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import '../navbar.css';
 
 function Navbar(){
 
     return(
-        <div>this is nav bar</div>
+        <div className="nav-menu">
+            <div className="nav-links">
+                <ul className="">
+                <li><a href="#" className="nav-text">home</a></li>
+                <li><a href="#" className="nav-text">about</a></li>
+                <li><a href="#" className="nav-text">porfolio</a></li>
+                <li><a href="#" className="nav-text">contact me</a></li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
