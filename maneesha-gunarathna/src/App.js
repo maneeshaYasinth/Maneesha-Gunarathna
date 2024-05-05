850451ad74634e9b0c80530653bac6d86fe4647c
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-05-05 19:40:44 +0530

    added files

---
+++ maneesha-gunarathna/src/App.js
@@ -0,0 +1,11 @@
+import Navbar from './components/navbar';
+import './App.css';
+
+function App() {
+  return (
+    
+    <Navbar />
+  );
+}
+
+export default App;
---
+++ maneesha-gunarathna/src/components/navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Navbar(){
+
+    return(
+        <div>this is nav bar</div>
+    )
+}
+
+export default Navbar;
