f24ad92f1c0268f8f07dc0f8d1255860d7206665
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-05-11 00:42:19 +0530

    updated

--- maneesha-gunarathna/src/components/about.jsx
+++ maneesha-gunarathna/src/components/about.jsx
@@ -9,7 +9,7 @@ const About = () => {
     <div>
       <h2 className='aboutMe-title'>About me</h2>
       <div className="aboutMe-discription">
-      <p>My name is Maneesha Yasinth Gunarathna, and I'm a passionate programmer with a keen interest in software development and electronics. I'm currently pursuing an undergraduate degree in BSc .Hons in Electronics and Computer Science at the University of Kelaniya. I specialize in developing innovative solutions that bridge the gap between software and hardware. From designing embedded systems to implementing web applications, I strive to create cutting-edge projects that make a positive impact on society. If you're interested in collaborating on exciting projects or discussing the latest trends in tech, feel free to connect with me.</p>
+      <p>My name is Maneesha Yasinth Gunarathna, and I'm a passionate programmer with a keen interest in software development and electronics. I'm currently pursuing an undergraduate degree in BSc .(Hons) in Electronics and Computer Science at the University of Kelaniya. I specialize in developing innovative solutions that bridge the gap between software and hardware. From designing embedded systems to implementing web applications, I strive to create cutting-edge projects that make a positive impact on society. If you're interested in collaborating on exciting projects or discussing the latest trends in tech, feel free to connect with me.</p>
       </div>
       
     </div>
