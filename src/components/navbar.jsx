abe637a617444516ef1f426e9cec758ad55eabfa
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-06-09 00:19:39 +0530

    styled the navbar and sidebar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="fixed top-0 left-0 z-50 w-full shadow-lg navbar font-krona bg-gradient-to-r from-pink-600 to-yellow-400 rounded-lg">
+    <nav className="fixed top-0 left-0 z-50 w-full shadow-lg navbar font-krona bg-gradient-to-r from-purple-700 to-pink-700 rounded-lg">
       <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8 ">
         <div className="relative flex items-center justify-center h-16 ">
           {/* Nav Links - Desktop */}
@@ -41,7 +41,7 @@ const Navbar = () => {
       {isOpen && (
         <div className="fixed inset-0 z-50 flex">
           {/* Sidebar Content */}
-          <div className="w-full h-full bg-purple-600 p-4 space-y-4 transition-transform duration-300 ease-in-out transform backdrop-filter backdrop-blur-lg shadow-lg">
+          <div className="w-full h-full bg-gradient-to-r from-purple-700 to-pink-700 p-4 space-y-4 transition-transform duration-300 ease-in-out transform backdrop-filter backdrop-blur-lg shadow-lg">
             {/* Close Button */}
             <div className="flex items-center justify-between">
               <button
@@ -77,11 +77,17 @@ const Navbar = () => {
 const NavLink = ({ href, children }) => {
   return (
     <a
-      href={href}
-      className="w-full px-4 py-2 text-lg font-medium text-white rounded-full hover:bg-gradient-to-b from-purple-700 to-fuchsia-950 hover:text-pink-100"
-    >
-      {children}
-    </a>
+  href={href}
+  className="relative w-full px-4 py-2 text-lg font-medium text-white rounded-full 
+             hover:text-pink-100
+             after:content-[''] after:absolute after:w-full after:h-0.5 after:bg-yellow-500 
+             after:left-0 after:bottom-0 after:transition-transform after:duration-300 
+             after:scale-x-0 hover:after:scale-x-100 
+             after:shadow-[0_0_10px_gold,0_0_20px_gold,0_0_30px_gold]"
+>
+  {children}
+</a>
+
   );
 };
 
