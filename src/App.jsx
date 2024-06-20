072c56e20103d129caa0c36389c3a85a9dde8b5e
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-06-21 00:35:04 +0530

    updated home page (some issues)

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
 
   return (<>
     <Navbar />
-    <Home />
+     <Home /> 
+    
+    
    
     </>
   )
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,20 +1,41 @@
 import React from 'react';
-import homeimg from '../assets/ne1.jpg'; 
+import homeimg from '../assets/ne1.jpg';
+import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa';
 
-const Home = () => {
+function Home() {
   return (
-   
-      <div className="flex flex-col md:flex-row items-center justify-center mt-32 max-w-6xl mx-auto">
-        <div className="flex-1 p-6 text-center md:text-left">
-          <h1 className="text-3xl md:text-5xl font-bold mb-4">Welcome to <strong className='text-customOrange'>MANEESHA</strong>'s Portfolio</h1>
-          <p className="text-lg md:text-xl">Hi, I'm Maneesha, a developer passionate about innovation. Explore my projects and skills. Let's create something amazing together!</p>
-        </div>
-        <div className="flex-1 flex justify-center md:justify-end mt-6 md:mt-0">
-          <img src={homeimg} alt="Profile" className="rounded-lg w-full max-w-sm" />
-        </div>
+    <section id="home" className="flex flex-col justify-center gap-5 text-center h-screen min-h-[500px] md:flex-row-reverse md:items-center md:justify-evenly p-4">
+      <div className="relative w-full md:w-auto">
+        <img
+          src={homeimg}
+          className="max-w-full h-auto mx-auto md:max-w-[250px] lg:max-w-[300px] object-cover"
+          alt="Profile picture of Maneesha Gunarathna"
+        />
       </div>
-    
+      <div className="flex flex-col gap-5 w-full md:w-auto">
+        <h1 className="text-4xl font-bold">
+          Maneesha
+          <br />
+          Gunarathna
+        </h1>
+        <h2 className="text-2xl">Frontend Developer</h2>
+        <span className="flex justify-center gap-6">
+          <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+            <FaTwitter className="w-7 h-7" />
+          </a>
+          <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+            <FaGithub className="w-7 h-7" />
+          </a>
+          <a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="w-7 h-7" />
+          </a>
+        </span>
+        <p className="max-w-[24ch] mx-auto">
+          Hi, I'm Maneesha, a developer passionate about innovation. Explore my projects and skills. Let's create something amazing together!
+        </p>
+      </div>
+    </section>
   );
-};
+}
 
 export default Home;
