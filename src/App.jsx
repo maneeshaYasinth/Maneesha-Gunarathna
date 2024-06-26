b1b50981a696f1e5c1104db379b252f5b64f4672
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-06-26 22:23:02 +0530

    remove the plx that way dosen't work

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (<>
-  {/* <About /> */}
+  
     <Navbar />
      <Home /> 
-      
+      <About />
      <Achivements />
      <ContactForm />
      
