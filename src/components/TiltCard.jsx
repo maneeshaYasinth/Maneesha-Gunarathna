b75a242f77dff27e1929dddc695e1e697eab6fcf
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2024-06-28 10:00:53 +0530

    added tilt plx effect to the home page

---
+++ src/components/TiltCard.jsx
@@ -0,0 +1,29 @@
+// src/components/TiltCard.js
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+
+const TiltCard = ({ children, backgroundImage, backgroundColor, customStyles, ...props }) => (
+  <Tilt
+    glareEnable={true}
+    glareMaxOpacity={0.8}
+    glareColor="#ffffff"
+    glarePosition="bottom"
+    glareBorderRadius="20px"
+    {...props}
+  >
+    <div
+      className={`p-6 rounded-lg shadow-md backdrop-blur-md bg-opacity-50 ${customStyles}`}
+      style={{
+        backgroundImage: backgroundImage ? `url(${backgroundImage})` : undefined,
+        backgroundColor: backgroundColor || 'transparent',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      {children}
+    </div>
+  </Tilt>
+);
+
+export default TiltCard;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,15 @@
+// src/components/Home.js
 import React from 'react';
+import TiltCard from './TiltCard'; // Import the TiltCard component
 import homeimg from '../assets/ne1.jpg';
 import { FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa';
 import background from '../assets/backgroundss.jpg';
-import bg from '../assets/bg.png';
-import text_img from '../assets/text-img.webp';
 
 function Home() {
   return (
-    <>
-      <section id="home" className="flex flex-col justify-center gap-5 text-center h-screen min-h-[500px] md:flex-row-reverse md:items-center md:justify-evenly p-4 bg-slate-500">
-        <div className="relative w-full md:w-auto flex justify-center">
+    <section id="home" className="flex flex-col justify-center gap-5 text-center h-screen min-h-[500px] p-4 bg-slate-900 ">
+      <TiltCard backgroundImage={background} backgroundColor="rgba(0, 0, 0, 0.5)" customStyles="w-full " className='mt-12'>
+        <div className="relative w-full flex justify-center">
           <div className="relative w-64 h-64">
             {/* Animated Blob */}
             <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-customOrange to-amber-700 rounded-full filter blur-xl opacity-70 animate-blob"></div>
@@ -21,7 +21,7 @@ function Home() {
             />
           </div>
         </div>
-        <div className="flex flex-col gap-5 w-full md:w-1/2">
+        <div className="flex flex-col gap-5 w-full md:w-1/2 mx-auto">
           <h1 className="text-6xl text-customOrange font-bold">
             Maneesha
             <br />
@@ -43,8 +43,8 @@ function Home() {
             Hi, I'm Maneesha, a developer passionate about innovation. Explore my projects and skills. Let's create something amazing together!
           </p>
         </div>
-      </section>
-    </>
+      </TiltCard>
+    </section>
   );
 }
 
