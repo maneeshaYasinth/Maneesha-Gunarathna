43702a7ee55c3de430d0e1e1ceef40761f09bd57
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2025-04-12 23:35:50 +0530

    modified the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import Navbar from './components/navbar'
 import Home from './components/home'
-import Achivements from './components/achivements'
 import ContactForm from './components/contact'
 import About from './components/about'
-import Education from './components/Education'
 import EducationPage from './pages/EducationPage'
 import { Route, Routes } from 'react-router-dom';
 
@@ -15,9 +13,9 @@ function App() {
     <Navbar />
     <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/education" element={<EducationPage />} />
+        <Route path="/Education" element={<EducationPage />} />
         <Route path="/About" element={<About />} />
-        <Route path="/ContactForm" element={<ContactForm />} />
+        <Route path="/Contact" element={<ContactForm />} />
       </Routes>
 
     </>
