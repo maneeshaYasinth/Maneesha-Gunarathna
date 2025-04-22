e5ace5ce78032bef959da9ec9c3ad61b4ae6cc2e
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2025-04-22 21:08:10 +0530

    added new page to compine positions and badges

---
+++ src/pages/ExperiencePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Experience from '../components/Experience'
+import Badges from '../components/Badges'
+
+const ExperiencePage = () => {
+  return (
+    <>
+        
+        <Experience />
+        <Badges />
+    </>
+  )
+}
+
+export default ExperiencePage
