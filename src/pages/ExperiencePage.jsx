db7431e43bbfebea10f3965c452cd1a551004358
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2025-05-01 11:24:33 +0530

    added the bg for this

--- src/pages/ExperiencePage.jsx
+++ src/pages/ExperiencePage.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import Experience from '../components/Experience'
-import Badges from '../components/Badges'
+import React from 'react';
+import Experience from '../components/Experience';
+import Badges from '../components/Badges';
+import TrailingDotGrid from '../components/TrailingDotGrid';
 
 const ExperiencePage = () => {
   return (
-    <>
-        
-        <Experience />
-        <Badges />
-    </>
-  )
-}
+    <div className="relative min-h-screen bg-black text-white overflow-hidden">
+      {/* Background applied once for the whole page */}
+      <div className="absolute inset-0 z-10 pointer-events-none">
+        <TrailingDotGrid className="w-full h-full" />
+      </div>
 
-export default ExperiencePage
+      {/* Content sections */}
+      <Experience />
+      <Badges />
+    </div>
+  );
+};
+
+export default ExperiencePage;
